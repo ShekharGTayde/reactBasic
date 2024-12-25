794fa3168a19682bdf2a13e8032ea5ac6d18697b
Shekhar Tayde <shekhartayde410@gmail.com>
2024-12-25 21:18:09 +0530

    revised

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -2,7 +2,7 @@ import Chai from './Chai';
 import React, { useState } from 'react';
 
 function App() {
-  const [color,setColor]=useState("black");
+  const [color,setColor]=useState("black"); // hooks is uesd to manage and update the states on ui 
   function RED(){
     setColor(color => "red");
   }
@@ -19,7 +19,7 @@ function App() {
     <>
     <div style={{color:color}}>
         <h3>SHEKHAR TAYDE</h3>
-        <button onClick={RED}>RED</button>
+         <button onClick={RED}>RED</button>  
         <button onClick={BLUE}>BLUE</button>
         <button onClick={GREEN}>GREEN</button>
         <button onClick={YELLOW}>YELLOW</button>
